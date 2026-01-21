dad408970c75d8c8c855cf10c957fea7abe7c8aa
izzy <radiotex777@gmail.com>
2026-01-21 13:37:31 -0500

    fixed progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -9,7 +9,7 @@ def brain_progression(username):
     while i < 3:
         rand_num = random.randint(0, 50)
         rand_num_diffrence = random.randint(1, 10)
-        rand_num_times = random.randint(5, 10)
+        rand_num_times = random.randint(6, 10)
         j = 0
         list_of_num = []
         while j != rand_num_times:
@@ -19,7 +19,7 @@ def brain_progression(username):
         index = random.randint(0, len(list_of_num) - 1)
         rand_element = list_of_num[index]
         list_of_num[index] = '..'
-        print(f"Question: {list_of_num}")
+        print(f"Question: {" ".join(list_of_num)}")
         answer = prompt.string('Your answer:')
         if answer == str(rand_element):
             print('Correct!')
